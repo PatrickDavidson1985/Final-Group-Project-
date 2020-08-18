bcad5d0bd9a74e2ceacd101dc066cb8933825168
Susie Q <66321114+Susie-w-Q@users.noreply.github.com>
2020-08-17 20:49:39 -0400

    Add files via upload

---
+++ final_final_project/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rescue Me!</title>
+  <link rel="stylesheet" href="finalproject.css">
+  <script src="https://kit.fontawesome.com/ffcfe413d5.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+<?php include("nav.php"); ?>
+        
+
+       <!--CARDS FOR DATABASE INPUT feel free to change images-->
+
+  <div class="container">
+
+    <div class="carousel">
+      <h1>Welcome to Rescue Me!</h1>
+      <p class="sub-heading">An All In One Safe Place to Rehome and Adopt Man's (And Woman's) Best Friend!</p>
+    </div>
+
+    <div class="card-groups-container">
+      <div class="card-deck">
+        <div class="card">
+          <img class="card-img-top" src="https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074_1280.jpg"
+            alt="image of cocker spaniel dog">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+              content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+
+        <div class="card">
+          <img class="card-img-top"
+            src="https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="image of a chocolate lab licking his nose">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+              content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="https://cdn.pixabay.com/photo/2015/03/26/09/54/pug-690566__340.jpg"
+            alt="image of a puppy pug on a hardwood floor">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card-deck second">
+        <div class="card">
+          <img class="card-img-top" src="https://cdn.pixabay.com/photo/2019/07/30/05/53/dog-4372036__340.jpg"
+            alt="image of a golden retriever puppy laying on the grass ">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+              content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/01/19/16/50/border-collie-1149417_1280.jpg"
+            alt="image of a black and white border collie in the snow">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top"
+            src="https://images.unsplash.com/photo-1577162365979-30fe9f9244aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+            alt="image of a charp lounging in a shar pei">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+              content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+      <!--END CARDS-->
+
+
+
+
+
+<?php include("footer.php"); ?>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ final_final_project/nav.php
@@ -0,0 +1,37 @@
+
+
+ <nav class="navbar navbar-expand-md bg-primary container-fluid-nav">
+    <a class="navbar-brand logo-position"></a> <img src="./images/logo-trans.png"
+      alt="image of logo, a paw print with text reading resuce me">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"><i class="fas fa-paw" style="color:white;"></i></span>
+    </button>
+
+    <div class="collapse navbar-collapse nav-position" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link text-white" href="index.php"><i class="fas fa-home">&nbsp;Home </i><span
+              class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="adoptioncenters.php"><i class="fas fa-user-friends">&nbsp;Adoption Centers</i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="adoptingparents.php"><i class="fas fa-dog">&nbsp;Adopting
+              Parents</i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="gallery.php"><i class="fas fa-image">&nbsp;Gallery</i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="aboutus.php"><i class="fas fa-bone">&nbsp;About Us</i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="adminpage.php"><i class="fas fa-search-plus">&nbsp;Admin Page</i></a>
+        </li>
+      </ul>
+     
+    </div>
+  </nav>
+
