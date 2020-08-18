586db09e009beadd0885ecb0210f407cd97a85b0
osujac <66321114+osujac@users.noreply.github.com>
2020-08-18 19:59:42 -0400

    updated all titles

--- final_final_project/index.php
+++ final_final_project/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Rescue Me!</title>
+  <title>Home</title>
   <link rel="stylesheet" href="finalproject.css">
   <script src="https://kit.fontawesome.com/ffcfe413d5.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
