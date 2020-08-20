93fe7b36fc8003a465cd9983d1bcd8991b8734e5
osujac <66321114+osujac@users.noreply.github.com>
2020-08-19 21:21:49 -0400

    re-linked footer to all pages

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
+  <link rel="stylesheet" href="footer.css">
   <link rel="stylesheet" href="finalproject.css">
   <script src="https://kit.fontawesome.com/ffcfe413d5.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
