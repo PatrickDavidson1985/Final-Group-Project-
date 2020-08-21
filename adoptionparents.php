c765d6705d3f522b5fb3be331fba5231a19a8a41
Mike Murray <murray3031@yahoo.com>
2020-08-20 20:50:28 -0400

    mike commit

--- adoptionparents.php
+++ adoptionparents.php
@@ -50,8 +50,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-     // Validation of Fence
-     if (empty($_POST["fence"])) {
+    // Validation of Fence
+    if (empty($_POST["fence"])) {
         $fenceErr = "Required";
     } else {
         $fence = test_input($_POST["fence"]);
@@ -60,8 +60,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-      // Validation of Pets
-      if (empty($_POST["pets"])) {
+    // Validation of Pets
+    if (empty($_POST["pets"])) {
         $petsErr = "Required";
     } else {
         $pets = test_input($_POST["pets"]);
@@ -71,12 +71,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 }
 ?>
 
@@ -92,160 +92,158 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="./styles/form-app.css">
     <link rel="stylesheet" href="./styles/footer.css">
     <link rel="stylesheet" href="./styles/finalproject.css">
-   
+
     <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/ffcfe413d5.js" crossorigin="anonymous"></script>
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Form Page</title>
 </head>
 
 <body>
-<?php include("nav.php"); ?>
+    <?php include("nav.php"); ?>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
-  
- <div id="form">   
 
-        <h1>Application Form</h1>
-        <br>
+        <div id="form">
 
-        <h4 class="required"><i class="fas fa-paw"></i> Required</h4>
-        <br>
-        <br>
-        <div id="name">
-            <div class="inputBox">
-                <input type="text" class="box" name="name" value="<?php echo $name; ?>" placeholder="Your Name">
-                <span class="error"><i class="fas fa-paw"></i></span> <?php echo $nameErr; ?></span>
+            <h1>Application Form</h1>
+            <br>
+
+            <h4 class="required"><i class="fas fa-paw"></i> Required</h4>
+            <br>
+            <br>
+            <div id="name">
+                <div class="inputBox">
+                    <input type="text" class="box" name="name" value="<?php echo $name; ?>" placeholder="Your Name">
+                    <span class="error"><i class="fas fa-paw"></i></span> <?php echo $nameErr; ?></span>
+                </div>
             </div>
-        </div>
-        <br>
+            <br>
 
-        <div id="address">
-            <input type="text" class="box" name="address" placeholder="Street Address" />
-            <span class="error"><i class="fas fa-paw"></i></span>
+            <div id="address">
+                <input type="text" class="box" name="address" placeholder="Street Address" />
+                <span class="error"><i class="fas fa-paw"></i></span>
                 <input type="text" class="box" name="city" placeholder="City" />
                 <span class="error"><i class="fas fa-paw"></i></span>
 
-
-                    <select name="state" class="state">
-                        <option value="AL">Alabama</option>
-                        <option value="AK">Alaska</option>
-                        <option value="AZ">Arizona</option>
-                        <option value="AR">Arkansas</option>
-                        <option value="CA">California</option>
-                        <option value="CO">Colorado</option>
-                        <option value="CT">Connecticut</option>
-                        <option value="DE">Delaware</option>
-                        <option value="DC">District Of Columbia</option>
-                        <option value="FL">Florida</option>
-                        <option value="GA">Georgia</option>
-                        <option value="HI">Hawaii</option>
-                        <option value="ID">Idaho</option>
-                        <option value="IL">Illinois</option>
-                        <option value="IN">Indiana</option>
-                        <option value="IA">Iowa</option>
-                        <option value="KS">Kansas</option>
-                        <option value="KY">Kentucky</option>
-                        <option value="LA">Louisiana</option>
-                        <option value="ME">Maine</option>
-                        <option value="MD">Maryland</option>
-                        <option value="MA">Massachusetts</option>
-                        <option value="MI">Michigan</option>
-                        <option value="MN">Minnesota</option>
-                        <option value="MS">Mississippi</option>
-                        <option value="MO">Missouri</option>
-                        <option value="MT">Montana</option>
-                        <option value="NE">Nebraska</option>
-                        <option value="NV">Nevada</option>
-                        <option value="NH">New Hampshire</option>
-                        <option value="NJ">New Jersey</option>
-                        <option value="NM">New Mexico</option>
-                        <option value="NY" selected=yes>New York</option>
-                        <option value="NC">North Carolina</option>
-                        <option value="ND">North Dakota</option>
-                        <option value="OH">Ohio</option>
-                        <option value="OK">Oklahoma</option>
-                        <option value="OR">Oregon</option>
-                        <option value="PA">Pennsylvania</option>
-                        <option value="RI">Rhode Island</option>
-                        <option value="SC">South Carolina</option>
-                        <option value="SD">South Dakota</option>
-                        <option value="TN">Tennessee</option>
-                        <option value="TX">Texas</option>
-                        <option value="UT">Utah</option>
-                        <option value="VT">Vermont</option>
-                        <option value="VA">Virginia</option>
-                        <option value="WA">Washington</option>
-                        <option value="WV">West Virginia</option>
-                        <option value="WI">Wisconsin</option>
-                        <option value="WY">Wyoming</option>
-
 <br>
-                        <input type="text" id="zipcode" class="box" name="zipcode" placeholder="Zip Code" maxlength="5" /><span class="error"><i class="fas fa-paw"></i></span> <?php echo $nameErr; ?></span>
-        </div>
-        <br>
+                <select name="state" class="state">
+                    <option value="AL">Alabama</option>
+                    <option value="AK">Alaska</option>
+                    <option value="AZ">Arizona</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="CA">California</option>
+                    <option value="CO">Colorado</option>
+                    <option value="CT">Connecticut</option>
+                    <option value="DE">Delaware</option>
+                    <option value="DC">District Of Columbia</option>
+                    <option value="FL">Florida</option>
+                    <option value="GA">Georgia</option>
+                    <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY" selected=yes>New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
 
-        <div id="email">
-            <div class="inputBox">
-                <input type="text" class="box" name="email" value="<?php echo $email; ?>" placeholder="Email Address">
-                <span class="error"><i class="fas fa-paw"></i></span> <?php echo $emailErr; ?></span>
-            </div>
-        </div>
-        <br>
-
-        <div id="number">
-            <div class="inputBox">
-                <input type="text" class="box" name="number" value="<?php echo $number; ?>" placeholder="Phone Number">
-                <span class="error"><i class="fas fa-paw"></i></span> <?php echo $numberErr; ?></span>
-            </div>
-        </div>
-        <br>
-
-
-        <div id="children">
-            <p>Children: <span class="error"><i class="fas fa-paw"></i></span> <?php echo $childErr; ?></span>
-                <ul><input type="radio" name="child" <?php if (isset($child)  && $child == "yes") echo "checked"; ?> value="yes"> Yes</ul>
-                <ul><input type="radio" name="child" <?php if (isset($child)  && $child == "no") echo "checked"; ?> value="no"> No</ul>
-
-        </div>
-        <br>
-
-        <div id="fence">
-            <p>Fenced Yard: <span class="error"><i class="fas fa-paw"></i></span> <?php echo $fenceErr; ?></span></p>
-            <ul><input type="radio" name="gender" <?php if (isset($fence)  && $fence == "yes") echo "checked"; ?> value="yes"> Yes</ul>
-            <ul><input type="radio" name="gender" <?php if (isset($fence)  && $fence == "no") echo "checked"; ?> value="no"> No</ul>
-
-        </div>
-        <br>
-        <br>
-        <div id="pets">
-            <p>Other Pets at Home: <span class="error"><i class="fas fa-paw"></i></span> <?php echo $petsErr; ?></span></p>
-            <ul><input type="radio" name="pets" <?php if (isset($pets)  && $pets == "yes") echo "checked"; ?> value="yes"> Yes</ul>
-            <ul><input type="radio" name="pets" <?php if (isset($pets)  && $pets == "no") echo "checked"; ?> value="no"> No</ul>
-            <div id="comment">
-                <p>If Yes, please describe pets at home:</p>
-                <textarea name="comment" class="ta" rows="5" cols="40" placeholder="Pets at home"><?php echo $comment; ?></textarea>
-            </div>
-            <div id="button">
-                <input type="submit" class="submit" name="submit" value="Submit">
-            </div>
-        </div>
+                    <br>
+                    </div>    
+                    <br>  
+<div> <input type="text" id="zipcode" class="zipcode" name="zipcode" placeholder="Zip Code" maxlength="5" /><span class="error"><i class="fas fa-paw"></i></span> <?php echo $nameErr; ?></span>
 </div>
+                    
+          
+            <br>
 
-<?php include("footer.php"); ?>
+            <div id="email">
+                <div class="inputBox">
+                    <input type="text" class="box" name="email" value="<?php echo $email; ?>" placeholder="Email Address">
+                    <span class="error"><i class="fas fa-paw"></i></span> <?php echo $emailErr; ?></span>
+                </div>
+            </div>
+            <br>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
+            <div id="number">
+                <div class="inputBox">
+                    <input type="text" class="box" name="number" value="<?php echo $number; ?>" placeholder="Phone Number">
+                    <span class="error"><i class="fas fa-paw"></i></span> <?php echo $numberErr; ?></span>
+                </div>
+            </div>
+            <br>
+
+
+            <div id="children">
+                <p>Children: <span class="error"><i class="fas fa-paw"></i></span> <?php echo $childErr; ?></span>
+                    <ul><input type="radio" name="child" <?php if (isset($child)  && $child == "yes") echo "checked"; ?> value="yes"> Yes</ul>
+                    <ul><input type="radio" name="child" <?php if (isset($child)  && $child == "no") echo "checked"; ?> value="no"> No</ul>
+
+            </div>
+            <br>
+
+            <div id="fence">
+                <p>Fenced Yard: <span class="error"><i class="fas fa-paw"></i></span> <?php echo $fenceErr; ?></span></p>
+                <ul><input type="radio" name="gender" <?php if (isset($fence)  && $fence == "yes") echo "checked"; ?> value="yes"> Yes</ul>
+                <ul><input type="radio" name="gender" <?php if (isset($fence)  && $fence == "no") echo "checked"; ?> value="no"> No</ul>
+
+            </div>
+            <br>
+            <br>
+            <div id="pets">
+                <p>Other Pets at Home: <span class="error"><i class="fas fa-paw"></i></span> <?php echo $petsErr; ?></span></p>
+                <ul><input type="radio" name="pets" <?php if (isset($pets)  && $pets == "yes") echo "checked"; ?> value="yes"> Yes</ul>
+                <ul><input type="radio" name="pets" <?php if (isset($pets)  && $pets == "no") echo "checked"; ?> value="no"> No</ul>
+                <div id="comment">
+                    <p>If Yes, please describe pets at home:</p>
+                    <textarea name="comment" class="ta" rows="5" cols="40" placeholder="Pets at home"><?php echo $comment; ?></textarea>
+                </div>
+                <div id="button">
+                    <input type="submit" class="submit" name="submit" value="Submit">
+                </div>
+            </div>
+        </div>
+
+        <?php include("footer.php"); ?>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
